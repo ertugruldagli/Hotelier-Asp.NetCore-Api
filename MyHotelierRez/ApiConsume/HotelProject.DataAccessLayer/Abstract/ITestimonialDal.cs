a9a2f75eb0bd46119a1858de260a46f5cf98898e
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-29 22:44:14 +0300

    some chanegs

--- MyHotelierRez/ApiConsume/HotelProject.DataAccessLayer/Abstract/ITestimonialDal.cs
+++ MyHotelierRez/ApiConsume/HotelProject.DataAccessLayer/Abstract/ITestimonialDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelProject.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.Abstract
 {
-    internal interface ITestimonialDal
+    public interface ITestimonialDal:IGenericDal<Testimonial>
     {
     }
 }
